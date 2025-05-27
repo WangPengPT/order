a7a5bbc3beff8a76b7a8e17e899580814489b5d7
WangPengPT <bbksoftai@gmail.com>
2025-05-27 17:21:02 +0100

    theme

--- client/src/main.js
+++ client/src/main.js
@@ -6,13 +6,101 @@ import App from './App.vue'
 
 import 'primeflex/primeflex.css';
 import PrimeVue from 'primevue/config';
+import { definePreset } from '@primeuix/themes';
 import Aura from '@primeuix/themes/aura';
 import ToastService from 'primevue/toastservice';
+
+const app_theme = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{orange.50}',
+            100: '{orange.100}',
+            200: '{orange.200}',
+            300: '{orange.300}',
+            400: '{orange.400}',
+            500: '{orange.500}',
+            600: '{orange.600}',
+            700: '{orange.700}',
+            800: '{orange.800}',
+            900: '{orange.900}',
+            950: '{orange.950}'
+        },
+        /*
+        * This code snippet comes from the AppConfigurator.vue file,
+        * from the getPresetExt function that returns this configuration,
+        I thought it would be prudent to bring it too
+        */
+        colorScheme: {
+            light: {
+                primary: {
+                    color: "{primary.500}",
+                    contrastColor: "#ffffff",
+                    hoverColor: "{primary.600}",
+                    activeColor: "{primary.700}",
+                },
+                highlight: {
+                    background: "{primary.50}",
+                    focusBackground: "{primary.100}",
+                    color: "{primary.700}",
+                    focusColor: "{primary.800}",
+                },
+                surface: {
+                    0: '#ffffff',
+                    50: '{slate.50}',
+                    100: '{slate.100}',
+                    200: '{slate.200}',
+                    300: '{slate.300}',
+                    400: '{slate.400}',
+                    500: '{slate.500}',
+                    600: '{slate.600}',
+                    700: '{slate.700}',
+                    800: '{slate.800}',
+                    900: '{slate.900}',
+                    950: '{slate.950}'
+                }
+            },
+            /*
+            * This code snippet comes from the AppConfigurator.vue file,
+            * from the getPresetExt function that returns this configuration,
+            I thought it would be prudent to bring it too
+            */
+            dark: {
+                primary: {
+                    color: "{primary.400}",
+                    contrastColor: "{surface.900}",
+                    hoverColor: "{primary.300}",
+                    activeColor: "{primary.200}",
+                },
+                highlight: {
+                    background: "color-mix(in srgb, {primary.400}, transparent 84%)",
+                    focusBackground:
+                        "color-mix(in srgb, {primary.400}, transparent 76%)",
+                    color: "rgba(255,255,255,.87)",
+                    focusColor: "rgba(255,255,255,.87)",
+                },
+                surface: {
+                    0: '#ffffff',
+                    50: '{slate.50}',
+                    100: '{slate.100}',
+                    200: '{slate.200}',
+                    300: '{slate.300}',
+                    400: '{slate.400}',
+                    500: '{slate.500}',
+                    600: '{slate.600}',
+                    700: '{slate.700}',
+                    800: '{slate.800}',
+                    900: '{slate.900}',
+                    950: '{slate.950}'
+                }
+            }
+        }
+    }
+});
 
 const app = createApp(App);
 app.use(PrimeVue, {
     theme: {
-        preset: Aura
+        preset: app_theme
     }
 });
 app.use(ToastService);
