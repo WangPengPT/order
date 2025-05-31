44d8468219765c25b780dfdca9a3c9593b54c433
WangPengPT <bbksoftai@gmail.com>
2025-05-31 16:37:33 +0100

    config

--- dist/manager.js
+++ dist/manager.js
@@ -1,8 +1,10 @@
+const config = require('config');
 const express = require('express');
+
 
 // 创建express应用
 const app = express();
-const port = 3001; // 设置端口号
+const port = config.get('app.port') + 2; // 设置端口号
 
 app.use(express.static('../manager/dist/spa'));
 
