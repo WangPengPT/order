d0b16188ed1a3cd0b240591361195f9cecc77459
Heng Zheng <hzgge1@iscte-iul.pt>
2025-06-23 15:00:41 +0100

    测试服

---
+++ build_teste.bat
@@ -0,0 +1,31 @@
+rem package, update used git
+rem server will auto update.
+
+rm dist -rf
+ 
+set "script_dir=%~dp0"
+echo %script_dir%
+
+set SRC_PATH=%script_dir%..\oneorder
+set DST_PATH=%script_dir%\teste_sushicome
+
+cd %SRC_PATH%
+xcopy .\server %DST_PATH% /E /Y /I
+
+
+cd manager
+call npm run build
+ren .\dist\spa\index.html manager.html
+xcopy .\dist\spa %DST_PATH%\public /E /Y /I
+
+
+cd ..
+cd client
+call npm run build
+ren .\dist\index.html client.html
+xcopy .\dist %DST_PATH%\public /E /Y /I
+
+
+cd %DST_PATH%
+
+
--- teste_sushicome/ecosystem.config.js
+++ teste_sushicome/ecosystem.config.js
@@ -1,7 +1,7 @@
 // ecosystem.config.js
 module.exports = {
   apps: [{
-    name: "test sushicome",
+    name: "order",
     script: "./server.js",
     instances: 1,
     autorestart: true,
@@ -9,8 +9,9 @@ module.exports = {
     max_memory_restart: "800M",
     env: {
       NODE_ENV: "production",
-      PORT: 800,
-      QR_ADDR: "https://order.xiaoxiong.pt:800/client.html?table=",
+      USE_HTTPS : true,
+      PORT: 443,
+      QR_ADDR: "https://order.xiaoxiong.pt/client.html?table=",
     }
   }]
 }
