7f95fbe208ffa264f849bfcdd6b16a324a8a511b
Heng Zheng <hzgge1@iscte-iul.pt>
2025-06-26 15:05:28 +0100

    配置文件

--- config files/marquespombal.js
+++ config files/marquespombal.js
@@ -9,7 +9,7 @@ module.exports = {
     max_memory_restart: "800M",
     env: {
       NODE_ENV: "production",
-      USE_HTTPS : false,
+      USE_HTTPS : true,
       PORT: 800,
       QR_ADDR: "https://order.xiaoxiong.pt:800/client.html?table=",
       TABLE_NUMBER: [[1,9],[21,26],[41,48],[63,67]],
--- config files/saldanha.js
+++ config files/saldanha.js
@@ -9,7 +9,7 @@ module.exports = {
     max_memory_restart: "800M",
     env: {
       NODE_ENV: "production",
-      USE_HTTPS : false,
+      USE_HTTPS : true,
       PORT: 600,
       QR_ADDR: "https://order.xiaoxiong.pt:600/client.html?table=",
       TABLE_NUMBER: [[2,25]],
