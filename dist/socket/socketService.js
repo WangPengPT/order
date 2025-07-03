8334b9e8dea0e39341bf9dc0f4283af450bbbb2b
GrapefruitKiwi <kaiweidong2001@gmail.com>
2025-07-03 20:05:55 +0100

    UI添加

--- dist/socket/socketService.js
+++ dist/socket/socketService.js
@@ -38,7 +38,6 @@ function init(io) {
       QR_ADDR: process.env.QR_ADDR,
       showRoastDuckPage: process.env.showRoastDuckPage,
       SAVE_ADDR: process.env.SAVE_ADDR,
-      RESERVE_URL: process.env.RESERVE_URL,
     });
 
     const tableSocket = new TableSocket(io)
