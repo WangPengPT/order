04c7d34d0203e64309f398f9d09bdca99d3f4223
WangPengPT <bbksoftai@gmail.com>
2025-07-04 17:24:19 +0100

    Update teste.config.js

--- bbq_configfiles/teste.config.js
+++ bbq_configfiles/teste.config.js
@@ -3,17 +3,17 @@ module.exports = {
   apps: [{
     name: "BBQ_DEMO",
     script: "./server.js",
-	cwd: "../dist",
+	cwd: "../dist_bbq",
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: "800M",
     env: {
       NODE_ENV: "production",
-      USE_HTTPS : true,
+      USE_HTTPS : false,
 	  HTTPS_HEAD: "sushi",
-      PORT: 10000,
-      QR_ADDR: "https://sushi.xiaoxiong.pt:10000/client.html?table=",
+      PORT: 8000,
+      QR_ADDR: "http://146.59.150.128:8000/client.html?table=",
       TABLE_NUMBER: [[1,50]],
       SAVE_ADDR: "save/demo1",
     }
