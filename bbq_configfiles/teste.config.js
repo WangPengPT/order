6d783fc20ee882defe33bcba30c9288a7e34518c
Heng Zheng <hzgge1@iscte-iul.pt>
2025-08-18 16:58:46 +0100

    Update teste.config.js

--- bbq_configfiles/teste.config.js
+++ bbq_configfiles/teste.config.js
@@ -3,20 +3,20 @@ module.exports = {
   apps: [{
     name: "BBQ_DEMO",
     script: "./server.js",
-    cwd: "../dist_bbq",
+	cwd: "../dist_bbq",
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: "800M",
     env: {
       NODE_ENV: "production",
-      USE_HTTPS: false,
-      HTTPS_HEAD: "sushi",
+      USE_HTTPS : true,
+	  HTTPS_HEAD: "v",
       PORT: 8000,
-      QR_ADDR: "http://146.59.150.128:8000/client.html?table=",
-      TABLE_NUMBER: [[1, 50]],
-      SAVE_ADDR: "save/demo1",
-      TEST_ENVIRONMENT: true,
+      QR_ADDR: "http://v.xiaoxiong.pt:8000/client.html?table=",
+      TABLE_NUMBER: [[1,50]],
+      SAVE_ADDR: "save/bbq_demo",
+	  TEST_ENVIRONMENT: true,
     }
   }]
 }
