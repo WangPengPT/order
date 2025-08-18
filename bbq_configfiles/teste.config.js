a6e4477a48c52ef176fdea72a9365769400f7e6a
GrapefruitKiwi <kaiweidong2001@gmail.com>
2025-08-18 16:50:23 +0100

    打包和清除

--- bbq_configfiles/teste.config.js
+++ bbq_configfiles/teste.config.js
@@ -3,19 +3,20 @@ module.exports = {
   apps: [{
     name: "BBQ_DEMO",
     script: "./server.js",
-	cwd: "../dist_bbq",
+    cwd: "../dist_bbq",
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: "800M",
     env: {
       NODE_ENV: "production",
-      USE_HTTPS : false,
-	  HTTPS_HEAD: "sushi",
+      USE_HTTPS: false,
+      HTTPS_HEAD: "sushi",
       PORT: 8000,
       QR_ADDR: "http://146.59.150.128:8000/client.html?table=",
-      TABLE_NUMBER: [[1,50]],
+      TABLE_NUMBER: [[1, 50]],
       SAVE_ADDR: "save/demo1",
+      TEST_ENVIRONMENT: true,
     }
   }]
 }
