5de84afec4f93fdbb39b50c567ab450b0575f78e
Heng Zheng <hzgge1@iscte-iul.pt>
2025-09-10 15:44:46 +0100

    Update teste.config.js

--- bbq_configfiles/teste.config.js
+++ bbq_configfiles/teste.config.js
@@ -17,7 +17,7 @@ module.exports = {
       ADDR: "https://v.xiaoxiong.pt:8000",
       TABLE_NUMBER: [[1,50]],
       SAVE_ADDR: "save/bbq_demo",
-	  TEST_ENVIRONMENT: true,
+	  TEST_ENVIRONMENT: false,
       JWT_SECRET: "my_jwt_secret_key"
     }
   }]
