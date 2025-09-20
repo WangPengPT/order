0e5ed1076b6782b46da4cb3620fa6b06deb72ea9
Heng Zheng <hzgge1@iscte-iul.pt>
2025-09-20 14:48:41 +0100

    Update teste.config.js

--- sushi_configfiles/teste.config.js
+++ sushi_configfiles/teste.config.js
@@ -14,6 +14,7 @@ module.exports = {
 	  HTTPS_HEAD: "v",
       PORT: 7000,
       QR_ADDR: "https://v.xiaoxiong.pt:7000/client.html?table=",
+	  ADDR: "https://v.xiaoxiong.pt:7000",
       TABLE_NUMBER: [[1,50]],
       SAVE_ADDR: "save/sc_teste",
       JWT_SECRET: "my_jwt_secret_key"
