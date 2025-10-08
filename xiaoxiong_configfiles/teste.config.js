d682203ad911c01e117a26ec55ef6d63eea67a71
kiwi <kaiweidong2001@gmail.com>
2025-10-08 16:23:41 +0100

    Update teste.config.js

--- xiaoxiong_configfiles/teste.config.js
+++ xiaoxiong_configfiles/teste.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apps: [{
     name: "XX_TEST",
     script: "./server.js",
-	cwd: "../dist_xiaoxiong",
+	cwd: "../dist",
     instances: 1,
     autorestart: false,
     watch: false,
@@ -14,10 +14,10 @@ module.exports = {
 	  HTTPS_HEAD: "v",
       PORT: 5000,
       QR_ADDR: "https://v.xiaoxiong.pt:5000/client.html?table=",
+      ADDR: "https://v.xiaoxiong.pt:5000",
       TABLE_NUMBER: [[1,50]],
       SAVE_ADDR: "save/xx_test",
-	  ENABLE_ROAST_DUCK: true,
-	  ENABLE_MENU_LUNCH: true,
+      JWT_SECRET: "my_jwt_secret_key"
     }
   }]
 }
