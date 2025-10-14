cdd3a65f113b449faf41a83ec2c13cc269fd1280
Heng Zheng <hzgge1@iscte-iul.pt>
2025-10-14 19:55:30 +0100

    小熊更新

--- xiaoxiong_configfiles/odivelas.config.js
+++ xiaoxiong_configfiles/odivelas.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apps: [{
     name: "XX_ODIVELAS",
     script: "./server.js",
-    cwd: "../dist_xiaoxiong",
+    cwd: "../dist",
     instances: 1,
     autorestart: true,
     watch: false,
@@ -14,10 +14,11 @@ module.exports = {
       HTTPS_HEAD: "order",
       PORT: 503,
       QR_ADDR: "https://order.xiaoxiong.pt:503/client.html?table=",
+	  ADDR: "https://order.xiaoxiong.pt:503",
+	  ADMIN_PASS: "1015",
       TABLES_NUMBER: [[2,14],[19,26],[33,38]],
       SAVE_ADDR: "save/xx_odivelas",
-      ENABLE_ROAST_DUCK: false,
-	  ENABLE_MENU_LUNCH: false,
+	  JWT_SECRET: "my_jwt_secret_key"
     }
   }]
 }
